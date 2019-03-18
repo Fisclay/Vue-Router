e61873b5bccab64aff7940eb95139cda0f757128
Fisclay <47273168+Fisclay@users.noreply.github.com>
2019-03-18 15:19:58 +0700

    Add files via upload

---
+++ Vue_Router/src/components/Siswa.vue
@@ -0,0 +1,29 @@
+<template>
+ <div><br>
+  <center>
+  <table border="1">
+    <tr>
+      <td><center><b>Kelas</b></center></td>
+      <td><center><b>Nama</b></center></td>
+    </tr>
+    <tr>
+      <td>XII - RPL 1</td>
+      <td>Mohammad Nafis Putra L</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 2</td>
+      <td>Attar Firmansyah</td>
+    </tr>
+    <tr>
+      <td>XII - RPL 3</td>
+      <td>Dimas Rahayu</td>
+    </tr>
+  </table>
+  </center>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
